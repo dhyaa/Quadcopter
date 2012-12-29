902dd974e769595aa1ae0ac59aa6940f433696e2
Aaron <Aaron.Durant82@gmail.com>
2012-12-29 11:56:16 +0000

    Imported Code

---
+++ source/Quadcopter_main.cpp
@@ -0,0 +1,19 @@
+#include "Quadcopter_main.h"
+
+/*************************************************************
+project: Quadcopter
+author: Aaron Durant
+description: Contains setup and loop functions for program
+*************************************************************/
+
+#include "Quadcopter_main.h"
+
+void setup() 
+{                
+     
+}
+
+void loop() 
+{
+            
+}
